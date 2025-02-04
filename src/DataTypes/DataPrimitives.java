51cfe91097bb9eee180581368ad9181cac3f807f
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-02-04 15:42:06 -0300

    feat: add primitive data

---
+++ src/DataTypes/DataPrimitives.java
@@ -0,0 +1,15 @@
+package DataTypes;
+
+public class DataPrimitives {
+    public static void main(String[] args) {
+        String name = "Naruto";
+        char initialLetterName = 'N';
+        int age = 16; // Int support max: 2_147_483_647
+        double height = 1.68;
+        boolean isLife = true;
+        Long balance = 2_450_456_545L;
+        byte jutsuAmount = 10;
+        short friends = 1_456;
+        float strength = 100.4F;
+    }
+}
