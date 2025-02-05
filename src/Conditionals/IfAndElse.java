3bbe0c9d165c19a76f29b2042e2fa1a7eff63757
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-02-05 15:16:01 -0300

    feat: add if, else if and else

---
+++ src/Conditionals/IfAndElse.java
@@ -0,0 +1,20 @@
+package Conditionals;
+
+public class IfAndElse {
+    public static void main(String[] args) {
+        String name = "Naruto Uzumaki";
+        String ranking;
+        int age = 16;
+        boolean isHokage = false;
+        short amountMissions = 10;
+
+        if (amountMissions >= 10 && age >= 15){
+            System.out.println("Rank: Chunnin");
+        } else if (amountMissions >= 20) {
+            System.out.println("Rank: Jounin");
+
+        } else {
+            System.out.println("Rank: Gennin");
+        }
+    }
+}
