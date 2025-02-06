c4167949c2841d36638c93894cab4924ecbd58b1
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-02-06 15:49:32 -0300

    feat: add switch cases

---
+++ src/Conditionals/SwitchCases.java
@@ -0,0 +1,36 @@
+package Conditionals;
+
+import java.util.Scanner;
+
+public class SwitchCases {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Chose a character: ");
+        System.out.println("1 - Naruto Uzumaki");
+        System.out.println("2 - Sasuke Uchiha");
+        System.out.println("3 - Sakura Haruno");
+
+
+        int userChoice = userInput.nextInt();
+
+        System.out.println("You enter with number " + userChoice);
+
+        switch (userChoice){
+            case 1:
+                System.out.println("You chose Naruto Uzumaki");
+                break;
+            case 2:
+                System.out.println("You chose Sasuke Uchiha");;
+                break;
+            case 3:
+                System.out.println("You chose Sakura Haruno");
+                break;
+            default:
+                System.out.println("You need provide a number with range is 1 a 3");
+        }
+
+
+        userInput.close();
+    }
+}
