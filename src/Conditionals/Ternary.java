be40cb7402f980ce143973997eb05baf032b39c6
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-02-06 16:06:13 -0300

    feat: add ternary

---
+++ src/Conditionals/Ternary.java
@@ -0,0 +1,10 @@
+package Conditionals;
+
+public class Ternary {
+    public static void main(String[] args) {
+        short missionsAmount = 11;
+        String level = (missionsAmount >= 10) ? "This ninja has more than 10 mission" : "This ninja has less than 10 missions";
+
+        System.out.println(level);
+    }
+}
