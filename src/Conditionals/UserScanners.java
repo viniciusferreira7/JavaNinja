5265346b39e1587e324127d33faad4b336f04883
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-02-06 15:18:28 -0300

    feat: add class to use scanners

---
+++ src/Conditionals/UserScanners.java
@@ -0,0 +1,27 @@
+package Conditionals;
+
+import java.util.Scanner;
+
+public class UserScanners {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter with your ninja name: ");
+        String ninjaName = input.nextLine();
+
+        System.out.println("Enter with your age: ");
+        int age = input.nextInt();
+
+        System.out.println("age = " + age + " years");
+        System.out.println("ninjaName = " + ninjaName);
+
+        if(age >= 18){
+            System.out.println("This ninja is order and he can go to outside of village to do missions");
+        } else {
+            System.out.println("This ninja is still very young, you must to train more, before you go out of village");
+        }
+
+        input.close();
+
+    }
+}
