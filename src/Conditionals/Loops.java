961cc8dc51595bbe7cb1c378d3cca4777c8e4dde
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-02-07 15:20:17 -0300

    feat: add loops while and for

---
+++ src/Conditionals/Loops.java
@@ -0,0 +1,18 @@
+package Conditionals;
+
+public class Loops {
+    public static void main(String[] args){
+        int kageBunshin = 0;
+        int maxKageBunshin = 40;
+
+        /*while(kageBunshin <= maxKageBunshin){
+            System.out.println("kageBunshin = " + kageBunshin);
+
+            kageBunshin++;
+        }*/
+
+        for (int i = 0; i <= maxKageBunshin; i++) {
+            System.out.println(i);
+        }
+    }
+}
