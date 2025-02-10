94c0282f6abf362d7beaa2d6b1a61513bf362acb
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-02-10 13:06:15 -0300

    feat: start array

--- src/BasicLevel/Array.java
+++ src/BasicLevel/Array.java
@@ -2,13 +2,26 @@ package BasicLevel;
 
 public class Array {
     public static void main(String[] args) {
-        String[] ninjas = new String[3];
-        int[] age = new int[3];
-        
+
+        // String starts with null
+        String[] ninjas = new String[4];
         ninjas[0] = "Naruto";
         ninjas[2] = "Sasuke";
         ninjas[1] = "Sakura";
+        System.out.println(ninjas[3]);
 
-        System.out.println("ninjas = " + ninjas);
+        // Int starts with 0
+        int[] age = new int[2];
+        age[0] = 13;
+        System.out.println(age[1]);
+
+        // Boolean starts with false
+        boolean[] isNinja = new boolean[2];
+        isNinja[0] = true;
+        System.out.println(isNinja[1]);
+
+        // Double starts with 0.0
+        double[] floatNumber = new double[2];
+        System.out.println(floatNumber[0]);
     }
 }
